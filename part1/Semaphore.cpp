4d2ac76a0bba7473f8226f3186a6ecdb26924a08
englender <ronien262@gmail.com>
2019-05-20 15:26:43 +0300

    google tests passed

--- part1/Semaphore.cpp
+++ part1/Semaphore.cpp
@@ -1,34 +1,38 @@
 
 #include "Semaphore.hpp"
+//#include <windows.h>
 
 #define LOCKED 0
 
 Semaphore::Semaphore() : semaphorMax(0), counter(0){
-    pthread_cond_init(&this->cond, nullptr);
-    pthread_mutex_init(&this->mutex, nullptr);          // NEED TO CHECK what attribute to add
+    pthread_cond_init(&cond, nullptr);
+    pthread_mutex_init(&mutex, nullptr);          // NEED TO CHECK what attribute to add
 }
 
-Semaphore::Semaphore(unsigned val) : semaphorMax(val), counter(val){}
+Semaphore::Semaphore(unsigned val) : semaphorMax(val), counter(val){
+    pthread_cond_init(&cond, nullptr);
+    pthread_mutex_init(&mutex, nullptr);
+}
 
 Semaphore::~Semaphore() {
-    pthread_cond_destroy(&this->cond);
-    pthread_mutex_destroy(&this->mutex);
+    pthread_cond_destroy(&cond);
+    pthread_mutex_destroy(&mutex);
 }
 
 void Semaphore::up(){
-    mutex_lock(&this->mutex);
-    if(this->counter < this->semaphorMax)
+    pthread_mutex_lock(&mutex);
+//    if(counter < semaphorMax)
         counter++;
 
-    cond_signal(&this->cond);
-    mutex_unlock(&this->mutex);
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&mutex);
 }
 
 void Semaphore::down() {
-    mutex_lock(&this->mutex);
-    while (this->counter <= 0)
-        cond_wait(&this->cond, &this->mutex);
-
+    pthread_mutex_lock(&mutex);
+    while (counter <= LOCKED) {
+        pthread_cond_wait(&cond, &mutex);
+    }
     counter--;
-    mutex_unlock(&this->mutex);
+    pthread_mutex_unlock(&mutex);
 }
