d1183776f6936214f142997dee40846481c2edaa
NadavOrzech <45461272+NadavOrzech@users.noreply.github.com>
2019-05-27 16:11:04 +0300

    pcq from cpp to hpp

--- PCQueue.cpp
+++ PCQueue.cpp
@@ -15,62 +15,62 @@
 //    this->p_count=0;
 //}
 
-template <class T>
-PCQueue<T>::~PCQueue() {
-    delete this->size;
-    delete this->consumer;
-    delete this->resource;
-    delete this->queue;
-    pthread_mutex_destroy(&cond_mutex);
-    pthread_mutex_destroy(&mutex);
-}
+//template <class T>
+//PCQueue<T>::~PCQueue() {
+//delete this->size;
+//delete this->consumer;
+//delete this->resource;
+//delete this->queue;
+//pthread_mutex_destroy(&cond_mutex);
+//pthread_mutex_destroy(&mutex);
+//}
 
-template <class T>
-T PCQueue<T>::pop(){
-    T retVal;
-    size.down();                            //preventing pop from empty queue
+//template <class T>
+//T PCQueue<T>::pop(){
+//    T retVal;
+//    size.down();                            //preventing pop from empty queue
+//
+//    pthread_mutex_lock(&cond_mutex);
+//    while(this->writer_lock==1 || this->reader_lock==1)
+//        pthread_cond_wait(&cond,&cond_mutex);
+//
+//    this->reader_lock = true;               //if there is no writer by this line, unable reading
+//    pthread_mutex_unlock(&cond_mutex);
+//
+////---------Start of the critical section--------
+//
+//    pthread_mutex_lock(&mutex);
+//    retVal=this->queue.front();
+//    this->queue.pop();
+//    pthread_mutex_unlock(&mutex);
+//
+////---------End of the critical section----------
+//
+//    pthread_mutex_lock(&cond_mutex);
+//    this->reader_lock = false;
+//    pthread_cond_broadcast(&cond_mutex);    //enabling the next tread to enter
+//    pthread_mutex_unlock(&cond_mutex);
+//
+//    return retVal;
+//}
 
-    pthread_mutex_lock(&cond_mutex);
-    while(this->writer_lock==1 || this->reader_lock==1)
-        pthread_cond_wait(&cond,&cond_mutex);
-
-    this->reader_lock = true;               //if there is no writer by this line, unable reading
-    pthread_mutex_unlock(&cond_mutex);
-
-//---------Start of the critical section--------
-
-    pthread_mutex_lock(&mutex);
-    retVal=this->queue.front();
-    this->queue.pop();
-    pthread_mutex_unlock(&mutex);
-
-//---------End of the critical section----------
-
-    pthread_mutex_lock(&cond_mutex);
-    this->reader_lock = false;
-    pthread_cond_broadcast(&cond_mutex);    //enabling the next tread to enter
-    pthread_mutex_unlock(&cond_mutex);
-
-    return retVal;
-}
-
-template <class T>
-void PCQueue<T>::push(const T& item){
-    pthread_mutex_lock(&cond_mutex);
-    this->writer_lock = true;
-    pthread_mutex_unlock(&cond_mutex);
-
-//---------Start of the critical section--------
-
-    pthread_mutex_lock(&mutex);
-    this->queue.push(item);
-    size.up();                              //allowing the next reader to pop
-    pthread_mutex_unlock(&mutex);
-
-//---------End of the critical section----------
-
-    pthread_mutex_lock(&cond_mutex);
-    this->writer_lock = false;
-    pthread_cond_broadcast(&cond_mutex);
-    pthread_mutex_unlock(&cond_mutex);
-}
+//template <class T>
+//void PCQueue<T>::push(const T& item){
+//    pthread_mutex_lock(&cond_mutex);
+//    this->writer_lock = true;
+//    pthread_mutex_unlock(&cond_mutex);
+//
+////---------Start of the critical section--------
+//
+//    pthread_mutex_lock(&mutex);
+//    this->queue.push(item);
+//    size.up();                              //allowing the next reader to pop
+//    pthread_mutex_unlock(&mutex);
+//
+////---------End of the critical section----------
+//
+//    pthread_mutex_lock(&cond_mutex);
+//    this->writer_lock = false;
+//    pthread_cond_broadcast(&cond_mutex);
+//    pthread_mutex_unlock(&cond_mutex);
+//}
--- PCQueue.hpp
+++ PCQueue.hpp
@@ -27,17 +27,68 @@ public:
         this->reader_lock=false;
 
 	}
-	~PCQueue();
+	~PCQueue(){
+        delete this->size;
+        delete this->consumer;
+        delete this->resource;
+        delete this->queue;
+        pthread_mutex_destroy(&cond_mutex);
+        pthread_mutex_destroy(&mutex);
+    }
 
 	// Blocks while queue is empty. When queue holds items, allows for a single
 	// thread to enter and remove an item from the front of the queue and return it. 
 	// Assumes multiple consumers.
-	T pop(); 
+	T pop(){
+        T retVal;
+        size.down();                            //preventing pop from empty queue
+
+        pthread_mutex_lock(&cond_mutex);
+        while(this->writer_lock==1 || this->reader_lock==1)
+            pthread_cond_wait(&cond,&cond_mutex);
+
+        this->reader_lock = true;               //if there is no writer by this line, unable reading
+        pthread_mutex_unlock(&cond_mutex);
+
+//---------Start of the critical section--------
+
+        pthread_mutex_lock(&mutex);
+        retVal=this->queue.front();
+        this->queue.pop();
+        pthread_mutex_unlock(&mutex);
+
+//---------End of the critical section----------
+
+        pthread_mutex_lock(&cond_mutex);
+        this->reader_lock = false;
+        pthread_cond_broadcast(&cond_mutex);    //enabling the next tread to enter
+        pthread_mutex_unlock(&cond_mutex);
+
+        return retVal;
+    }
 
 	// Allows for producer to enter with *minimal delay* and push items to back of the queue.
 	// Hint for *minimal delay* - Allow the consumers to delay the producer as little as possible.  
 	// Assumes single producer 
-	void push(const T& item); 
+	void push(const T& item){
+        pthread_mutex_lock(&cond_mutex);
+        this->writer_lock = true;
+        pthread_mutex_unlock(&cond_mutex);
+
+//---------Start of the critical section--------
+
+        pthread_mutex_lock(&mutex);
+        this->queue.push(item);
+        size.up();                              //allowing the next reader to pop
+        pthread_mutex_unlock(&mutex);
+
+//---------End of the critical section----------
+
+        pthread_mutex_lock(&cond_mutex);
+        this->writer_lock = false;
+        pthread_cond_broadcast(&cond_mutex);
+        pthread_mutex_unlock(&cond_mutex);
+    }
 
 
 
